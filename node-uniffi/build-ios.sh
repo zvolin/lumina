2f00fa04bc1e515a0fd0484bb8029c307632d914
zvolin <mac.zwolinski@gmail.com>
2025-10-23 21:55:58 +0200

    abc

--- node-uniffi/build-ios.sh
+++ node-uniffi/build-ios.sh
@@ -5,24 +5,15 @@ set -euxo pipefail
 cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
 
 # create swift bindings
+
 rm -rf ./bindings ./ios
 mkdir -p ./bindings
 mkdir -p ./ios
 mkdir -p ./bindings/Headers
 
-host_triple="$(rustc --version --verbose | grep host: | cut -d' ' -f 2)"
-
-cargo build \
-  --release \
-  --lib \
-  --bin uniffi-bindgen \
-  --target "$host_triple" \
-  --target aarch64-apple-ios \
-  --target aarch64-apple-ios-sim
-
-../target/"$host_triple"/release/uniffi-bindgen \
+cargo run --bin uniffi-bindgen \
   generate \
-  --library ../target/"$host_triple"/release/liblumina_node_uniffi.dylib \
+  --library ../target/debug/liblumina_node_uniffi.dylib \
   --language swift \
   --out-dir ./bindings
 
@@ -38,6 +29,13 @@ cp ./bindings/*.h ./bindings/Headers/
 rm -rf ./ios/lumina.xcframework
 
 # create xcode project
+
+cargo build \
+  --lib \
+  --release \
+  --target aarch64-apple-ios \
+  --target aarch64-apple-ios-sim
+
 xcodebuild -create-xcframework \
   -library ../target/aarch64-apple-ios/release/liblumina_node_uniffi.a -headers ./bindings/Headers \
   -library ../target/aarch64-apple-ios-sim/release/liblumina_node_uniffi.a -headers ./bindings/Headers \
